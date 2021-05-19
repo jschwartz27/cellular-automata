1279a72b79a289768d16a3c60ae5af369c115ae0
Joshua E. Schwartz <josh.schwartz92@gmail.com>
2021-05-19 00:09:23 -0500

    refactor: clean up code

--- terminal_fun.py
+++ terminal_fun.py
@@ -34,42 +34,42 @@ def conv(x):
     return "0" if x == " " else "1"
 
 
-def convB(x):
+def conv_binary(x):
     return SYMBOL if x == "1" else " "
 
 
-def automata(tRules, seed: bool) -> List[List[str]]:
-    L = os.get_terminal_size().columns - 1
+def automata(transition_rules, seed: bool) -> List[List[str]]:
+    terminal_length = os.get_terminal_size().columns - 1
 
     if seed:
-        cell = [" "] * (L - 1) + [SYMBOL]
+        cell = [" "] * (terminal_length - 1) + [SYMBOL]
     else:
         cell = list(map(
-            lambda x: random.choice([SYMBOL, " "]), range(L)))
+            lambda x: random.choice([SYMBOL, " "]), range(terminal_length)))
 
     it = [cell]
     for i in range(10000):
         print("".join(cell))
         time.sleep(.05)
-        nextC = list()
+        next_row = list()
         # because the index for loop can't wrap around
         q = cell[-1:] + cell[:2]
         q = "".join(list(map(lambda x: conv(x), q)))
-        k = convB(tRules[q])
-        nextC.append(k)
+        k = conv_binary(transition_rules[q])
+        next_row.append(k)
 
-        for i in range(1, L-1):
-            q = cell[i-1:i+2]
+        for j in range(1, terminal_length-1):
+            q = cell[j-1:j+2]
             q = "".join(list(map(lambda x: conv(x), q)))
-            k = convB(tRules[q])
-            nextC.append(k)
+            k = conv_binary(transition_rules[q])
+            next_row.append(k)
         # because the index for loop can't wrap around
         q = cell[-2:] + cell[:1]
         q = "".join(list(map(lambda x: conv(x), q)))
-        k = convB(tRules[q])
-        nextC.append(k)
+        k = conv_binary(transition_rules[q])
+        next_row.append(k)
 
-        cell = nextC
+        cell = next_row
         it.append(cell)
 
     return it
@@ -86,8 +86,9 @@ def main():
     # 111 110 101 100 011 010 001 000
     states = ['{0:03b}'.format(k) for k in range(8)][::-1]
     # transition rules
-    tRules = {states[i]: n_binary[i] for i in range(8)}
-    the_structure = automata(tRules, args["seed"])
+    transition_rules = {states[i]: n_binary[i] for i in range(8)}
+    the_structure = automata(transition_rules, args["seed"])
+
 
 if __name__ == '__main__':
     main()
